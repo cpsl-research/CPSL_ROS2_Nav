c4741e4a75c2978245322d2fb2db804c6f74a544
David Hunt <david.hunt@duke.edu>
2025-04-21 19:32:06 -0400

    updating nav2 configurations for custom namespaces and custom namespaced tf trees

--- src/cpsl_nav/launch/nav2_backup.launch.py
+++ src/cpsl_nav/launch/nav2_backup.launch.py
@@ -19,7 +19,8 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable,OpaqueFunction
 from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import (AndSubstitution, LaunchConfiguration,
+                                  NotSubstitution, PathJoinSubstitution)
 from launch_ros.actions import LoadComposableNodes, SetParameter
 from launch_ros.actions import Node, SetRemap, PushRosNamespace
 from launch_ros.descriptions import ComposableNode, ParameterFile
@@ -36,8 +37,11 @@ ARGUMENTS = [
         description='Top-level namespace'
     ),
     DeclareLaunchArgument('scan_topic', 
-        default_value='/scan',
+        default_value='scan',
         description='The LaserScan topic to use for slam'),
+    DeclareLaunchArgument('base_frame_id',
+        default_value='base_link',
+        description='The frame ID of the base_link frame (without tf pre-fix)'),
     DeclareLaunchArgument(
         'use_sim_time',
         default_value='false',
@@ -78,6 +82,7 @@ def launch_setup(context, *args,**kwargs):
 
     namespace = LaunchConfiguration('namespace')
     scan_topic = LaunchConfiguration('scan_topic')
+    base_frame_id = LaunchConfiguration('base_frame_id')
     use_sim_time = LaunchConfiguration('use_sim_time')
     autostart = LaunchConfiguration('autostart')
     params_file = LaunchConfiguration('params_file')
@@ -105,14 +110,39 @@ def launch_setup(context, *args,**kwargs):
     # https://github.com/ros/robot_state_publisher/pull/30
     # TODO(orduno) Substitute with `PushNodeRemapping`
     #              https://github.com/ros2/launch_ros/issues/56
-    remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+
+    namespace_str = namespace.perform(context)
+    base_frame_id_str = base_frame_id.perform(context)
+    if (namespace_str):
+        if not namespace_str.startswith('/'):
+            namespace_str = '/' + namespace_str
+        tf_prefix = namespace_str.strip("/")
+        odom_frame = "{}/odom".format(tf_prefix)
+        base_frame = "{}/{}".format(tf_prefix,base_frame_id_str)
+    else:
+        tf_prefix = ""
+        odom_frame = "odom"
+        base_frame = base_frame_id_str
+
+
+    scan_topic_str = scan_topic.perform(context)
+    scan_topic_str = scan_topic_str.strip("/")
 
     # Create our own temporary YAML files that include substitutions
-    param_substitutions = {'autostart': autostart}
+    params_str = params_file.perform(context)
+    nav_config_file = PathJoinSubstitution([pkg_cpsl_navigation, 'config', params_str])
+
+    #substituting parameters
+    param_substitutions = {
+        'autostart': autostart,
+        'scan_topic':scan_topic_str,
+        # 'odom_frame':odom_frame,
+        # 'robot_base_frame':base_frame
+    }
 
     configured_params = ParameterFile(
         RewrittenYaml(
-            source_file=params_file,
+            source_file=nav_config_file,
             root_key=namespace,
             param_rewrites=param_substitutions,
             convert_types=True,
@@ -120,22 +150,20 @@ def launch_setup(context, *args,**kwargs):
         allow_substs=True,
     )
 
-    namespace_str = namespace.perform(context)
-    if (namespace_str and not namespace_str.startswith('/')):
-        namespace_str = '/' + namespace_str
-
-
-    scan_topic_str = scan_topic.perform(context)
-
     stdout_linebuf_envvar = SetEnvironmentVariable(
         'RCUTILS_LOGGING_BUFFERED_STREAM', '1'
     )
 
+    # remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+    remappings = [
+        ('{}/map'.format(namespace_str),"/map"),
+        ('{}/goal_pose'.format(namespace_str),'/goal_pose')]
+
     load_nodes = GroupAction(
-        condition=IfCondition(PythonExpression(['not ', use_composition])),
         actions=[
             SetParameter('use_sim_time', use_sim_time),
-            SetRemap(namespace_str + '/scan', namespace_str + scan_topic_str),
+            # SetRemap(namespace_str + '/scan', namespace_str + scan_topic_str),
+            PushRosNamespace(namespace),
             Node(
                 package='nav2_controller',
                 executable='controller_server',
@@ -244,93 +272,7 @@ def launch_setup(context, *args,**kwargs):
                 parameters=[{'autostart': autostart}, {'node_names': lifecycle_nodes}],
             ),
         ],
-    )
-
-    # load_composable_nodes = GroupAction(
-    #     condition=IfCondition(use_composition),
-    #     actions=[
-    #         SetParameter('use_sim_time', use_sim_time),
-    #         LoadComposableNodes(
-    #             target_container=container_name_full,
-    #             composable_node_descriptions=[
-    #                 ComposableNode(
-    #                     package='nav2_controller',
-    #                     plugin='nav2_controller::ControllerServer',
-    #                     name='controller_server',
-    #                     parameters=[configured_params],
-    #                     remappings=remappings + [('cmd_vel', 'cmd_vel_nav')],
-    #                 ),
-    #                 ComposableNode(
-    #                     package='nav2_smoother',
-    #                     plugin='nav2_smoother::SmootherServer',
-    #                     name='smoother_server',
-    #                     parameters=[configured_params],
-    #                     remappings=remappings,
-    #                 ),
-    #                 ComposableNode(
-    #                     package='nav2_planner',
-    #                     plugin='nav2_planner::PlannerServer',
-    #                     name='planner_server',
-    #                     parameters=[configured_params],
-    #                     remappings=remappings,
-    #                 ),
-    #                 ComposableNode(
-    #                     package='nav2_behaviors',
-    #                     plugin='behavior_server::BehaviorServer',
-    #                     name='behavior_server',
-    #                     parameters=[configured_params],
-    #                     remappings=remappings + [('cmd_vel', 'cmd_vel_nav')],
-    #                 ),
-    #                 ComposableNode(
-    #                     package='nav2_bt_navigator',
-    #                     plugin='nav2_bt_navigator::BtNavigator',
-    #                     name='bt_navigator',
-    #                     parameters=[configured_params],
-    #                     remappings=remappings,
-    #                 ),
-    #                 ComposableNode(
-    #                     package='nav2_waypoint_follower',
-    #                     plugin='nav2_waypoint_follower::WaypointFollower',
-    #                     name='waypoint_follower',
-    #                     parameters=[configured_params],
-    #                     remappings=remappings,
-    #                 ),
-    #                 ComposableNode(
-    #                     package='nav2_velocity_smoother',
-    #                     plugin='nav2_velocity_smoother::VelocitySmoother',
-    #                     name='velocity_smoother',
-    #                     parameters=[configured_params],
-    #                     remappings=remappings
-    #                     + [('cmd_vel', 'cmd_vel_nav')],
-    #                 ),
-    #                 ComposableNode(
-    #                     package='nav2_collision_monitor',
-    #                     plugin='nav2_collision_monitor::CollisionMonitor',
-    #                     name='collision_monitor',
-    #                     parameters=[configured_params],
-    #                     remappings=remappings,
-    #                 ),
-    #                 ComposableNode(
-    #                     package='opennav_docking',
-    #                     plugin='opennav_docking::DockingServer',
-    #                     name='docking_server',
-    #                     parameters=[configured_params],
-    #                     remappings=remappings,
-    #                 ),
-    #                 ComposableNode(
-    #                     package='nav2_lifecycle_manager',
-    #                     plugin='nav2_lifecycle_manager::LifecycleManager',
-    #                     name='lifecycle_manager_navigation',
-    #                     parameters=[
-    #                         {'autostart': autostart, 'node_names': lifecycle_nodes}
-    #                     ],
-    #                 ),
-    #             ],
-    #         ),
-    #     ],
-    # )
-
-    
+    )    
 
     return [load_nodes]
 
